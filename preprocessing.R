3458c3c5c4ae359aabd2e3f4293348b2124cd094
Neo H <neo.hong@tu-dortmund.de>
2024-01-19 19:30:30 +0100

    Added an alternative function for extracting cabin data that doesn't use loops

--- preprocessing.R
+++ preprocessing.R
@@ -22,18 +22,81 @@ extract_cabin_data <- function(dataset) {
     dataset[["Deck"]] <- NA
     dataset[["Side"]] <- NA
     cabins <- dataset[["Cabin"]]
-    # side <- factor(levels = c(0, 1), labels = c("port", "starboard"))
-    for (current_cabins in cabins) {
-        if (current_cabins == "") next
-        cabins_list <- strsplit(current_cabins, " ")
-        deck <- statistical_mode(gsub("[[:digit:]]", "", cabins_list))
-        dataset[["Deck"]][cabins == current_cabins] <- ifelse(length(deck) == 1, deck, NULL)
-        starboard <- statistical_mode(as.logical(as.numeric(gsub("[^[:digit:]]", "", cabins_list)) %% 2))
-        dataset[["Side"]][cabins == current_cabins] <- ifelse(length(starboard) == 1, ifelse(starboard, "starboard", "port"), NULL)
-        print(deck)
-        print(starboard)
+    # contains all cabin entries that list more than one cabin
+    multiple_cabins <- unique(cabins[grepl(" ", cabins, fixed = TRUE)])
+    processed_cabins <- integer(length(multiple_cabins))
+    for (cabin_string in cabins[cabins != ""]) {
+        cabin <- cabin_string
+        # special case for handling multiple cabins for one person
+        if (cabin %in% multiple_cabins) {
+            # cabin <- mapply(setdiff, cabins_list, processed_cabins)[1]
+            cabins_list <- strsplit(cabin_string, " ")[[1]]
+            # check if we encountered this combination of cabins before,
+            # if yes, use whichever of these cabins hasn't been used yet
+            last_processed_cabin <-
+                processed_cabins[cabin_string == multiple_cabins]
+            # if all of the cabins were already used restart with the first
+            # one
+            processed_cabins[cabin_string == multiple_cabins] <-
+                ifelse(last_processed_cabin >= length(cabins_list),
+                    1, last_processed_cabin + 1
+                )
+            cabin <-
+                cabins_list[processed_cabins[cabin_string == multiple_cabins]]
+        }
+        deck <- gsub("[[:digit:]]", "", cabin)
+        dataset[["Deck"]][cabins == cabin_string] <- deck
+        starboard <- as.numeric(gsub("[^[:digit:]]", "", cabin)) %% 2
+        # uneven numbers %% 2 results in 1 which is interpreted as TRUE
+        dataset[["Side"]][cabins == cabin_string] <-
+            ifelse(starboard, "starboard", "port")
     }
-    # dataset[["Side"]] <- cabin
+    dataset[["Deck"]] <- factor(dataset[["Deck"]])
+    dataset[["Side"]] <- factor(dataset[["Side"]])
+    dataset
+}
+
+extract_cabin_data_2 <- function(dataset, loop = TRUE) {
+    dataset[["Deck"]] <- NA
+    dataset[["Side"]] <- NA
+    cabins <- as.character(dataset[["Cabin"]])
+
+
+    # deciding which cabin to use if there are multiple cabins
+
+    split_cabins <- strsplit(cabins, " ")
+    # contains all cabin entries that list more than one cabin
+    multiple_cabins <- unique(cabins[grepl(" ", cabins, fixed = TRUE)])
+    # the number of cabins in each row of the dataset
+    lengths <- sapply(split_cabins, length)
+    occurrences <- ave(seq_along(cabins), cabins, FUN = function(x) match(x, x))
+    if (loop) {
+        # if you reach the end of the list start again with the first element
+        # when this isn't done every index that goes out of bounds will be
+        # mapped to NA when applying the indices on split_cabins
+        occurrences <- (occurrences - 1) %% lengths + 1
+    }
+    indices <- ifelse(cabins %in% multiple_cabins, occurrences, 1)
+    # set each index of cabins to split_cabins[index][occurrences[index]]
+    table <- sapply(split_cabins, "[", indices)
+    cabins <- table[row(table) == col(table)]
+    dataset[["Cabins2"]] <- factor(cabins)
+
+
+    # extracting deck and side from the cabin
+
+    dataset[["Deck"]] <- factor(
+        gsub("[[:digit:]]", "", cabins)
+    )
+    # the numeric value 1 will be coerced to TRUE and 0 to FALSE
+    dataset[["Side"]] <-
+        factor(
+            ifelse(as.numeric(gsub("[^[:digit:]]", "", cabins)) %% 2,
+                "starboard", "port"
+            )
+        )
+
+
     dataset
 }
 
