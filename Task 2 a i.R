f36f10b0dfd27b8dae0a1fb307c716e092e35cff
helen-mza <156697847+helen-mza@users.noreply.github.com>
2024-02-16 13:28:01 +0100

    Add files via upload
    In task 2 a) i) we had to calculate the Suiting descriptive statistics for metric variables.

---
+++ Task 2 a i.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/Helen/Documents/TU Dortmund/Wissentschaftliches Arbeiten/Gruppenarbeit GITＨＵＢ/")
+data <- read.csv("titanic.csv")
+
+
+fun <- function(x){
+  data.frame(Min = min(x, na.rm=T), erstesQuartil =quantile(x, 0.25, na.rm=T) , 
+             Median = median(x, na.rm=T), Mean =mean(x, na.rm=T), 
+             SD = sd(x, na.rm=T), drittesQuartil =quantile(x, 0.75, na.rm=T),
+             Max = max(x, na.rm=T),
+             NAs = sum(is.na(x))
+             )
+}
+
+fun(data$Age)
+
