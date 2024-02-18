7cc529c522a19e5fccb00c8294131dbefeae27bd
Neo H <neo.hong@tu-dortmund.de>
2024-02-18 21:19:28 +0100

    Finished task 2a) ii and finished commenting

--- helpers.R
+++ helpers.R
@@ -1,8 +1,8 @@
 #' Returns the most frequently occuring value in x. If there are multiple values
 #' that occur most frequently this function returns all of them.
 #' @param x Can be a list, vector or factor.
-#' @param na.rm Defines if NA values should be removed.
-#' @return The statistical mode of x.
+#' @param na.rm Indicates if NA values should be removed before calculation.
+#' @return The statistical mode of x. One or multiple values.
 statistical_mode <- function(x, na.rm = TRUE) {
     if (na.rm) x <- na.omit(x)
     levels <- levels(x)
@@ -15,19 +15,26 @@ statistical_mode <- function(x, na.rm = TRUE) {
 #' occurs with the same frequency and 0 if only one level occurs 100% of the
 #' time.
 #' @param x A categorial variable.
+#' @param na.rm Indicates if NA values should be removed before calculation.
 #' @return A floating point number between 0 and 1.
 empirical_entropy <- function(x, na.rm = TRUE) {
     if (na.rm) x <- na.omit(x)
-    levels <- levels(x)
-    freq <- tabulate(match(x, levels)) / length(x)
+    freq <- tabulate(match(x, levels(x))) / length(x)
     1 / log(nlevels(x)) * sum(freq * log(1 / freq))
 }
 
+#' Computes the phi measure of dispersion which assumes more intuitive numbers
+#' between 0 and 1 but is not popular unlike entropy.
+#' @param x A categorial variable.
+#' @param na.rm Indicates if NA values should be removed before calculation.
+#' @return A floating point number between 0 and 1 (lower than entropy).
 phi_dispersion <- function(x, na.rm = TRUE) {
     if (na.rm) x <- na.omit(x)
     levels <- levels(x)
     freq <- tabulate(match(x, levels)) / length(x)
+    # The distance from a distribution with minimal dispersion
     phi_min <- 2 * (1 - max(freq))
+    # The distance from a distribution with maximal dispersion
     phi_max <- sum(abs(freq - 1 / nlevels(x)))
     phi_min / (phi_min + phi_max)
 }
