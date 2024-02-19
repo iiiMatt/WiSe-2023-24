9a3d6eb4b667b166a335eb1b7c074663c5172d53
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-19 12:52:06 +0100

    Adjust output to cat and list in utils.R

--- utils.R
+++ utils.R
@@ -16,24 +16,16 @@ chisq_test_plus_table <- function(kat_var1, kat_var2) {
   # chi-squared-test to output results
   chi_squared_result <- chisq.test(contingency_table)
 
-  # character vectors to explain the output of the function
-  hypothesis_explanation <- c(
-    "The results contain a contingency table and a
-        chi squared test regarding the following hypotheses:",
-    "Null Hypothesis (H0):
-        There is no association between the two categorical variables.",
-    "Alternative Hypothesis (H1):
-        There is an association between the two categorical variables."
-  )
-  result_explanation <- c(
-    "If p-value < alpha: Reject the null hypothesis.",
-    "If p-value >= alpha: Fail to reject the null hypothesis."
-  )
-  # return results in list
-  return(list(
-    hypothesis_explanation,
-    contingency_table = contingency_table,
-    chi_squared_result = chi_squared_result,
-    result_explanation
-  ))
+  #cat output to explain results/ output plus result on console
+  
+  cat("The results contain a contingency table and a chi squared test regarding the following hypotheses:\n
+      Null Hypothesis (H0): There is no association between the two categorical variables. \n
+      Alternative Hypothesis (H1): There is an association between the two categorical variables.\n
+      If p-value < alpha: Reject the null hypothesis. \n
+      If p-value >= alpha: Fail to reject the null hypothesis.")
+
+  
+  return( list(
+    contingency_table = contingency_table, 
+    chi_squared_result = chi_squared_result))
 }
