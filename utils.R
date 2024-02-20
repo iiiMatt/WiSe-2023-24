f7289eea325fbd45c7d180c714fcaf94bcf7b94d
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-20 10:02:33 +0100

    Update utils.R with deleted content of 2a - vi) in  analysis.R

--- utils.R
+++ utils.R
@@ -116,3 +116,16 @@ chisq_test_plus_table <- function(kat_var1, kat_var2) {
     contingency_table = contingency_table, 
     chi_squared_result = chi_squared_result))
 }
+
+#2a - vi)
+#Additional functions suitable for description and visualization
+Mosaikplot <- function(var1, var2){
+  mosaicplot(table(var1, var2 ), main = "Mosaic Plot", shade = TRUE, xlab = deparse(substitute(var1)) , ylab = deparse(substitute(var2)))
+}
+Mosaikplot(dataset$Survived, dataset$Age)
+
+Boxplots_with_Trendline <- function(var1, var2) {
+  plot(var1, var2, main = "Boxplot with Trendline",
+       xlab = deparse(substitute(var1)), ylab = deparse(substitute(var2)))
+  abline(lm(var2 ~ var1), col = "red")
+}
