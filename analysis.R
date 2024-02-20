b7f80516448799cda288336b2ad7e7494f696cf5
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-20 09:58:18 +0100

    Deleted content in analysis.R to move it to utils.R

--- analysis.R
+++ analysis.R
@@ -1,12 +1,1 @@
-#2a - vi)
-#Additional functions suitable for description and visualization
-Mosaikplot <- function(var1, var2){
-  mosaicplot(table(var1, var2 ), main = "Mosaic Plot", shade = TRUE, xlab = deparse(substitute(var1)) , ylab = deparse(substitute(var2)))
-}
-Mosaikplot(dataset$Survived, dataset$Age)
 
-Boxplots_with_Trendline <- function(var1, var2) {
-  plot(var1, var2, main = "Boxplot with Trendline",
-       xlab = deparse(substitute(var1)), ylab = deparse(substitute(var2)))
-  abline(lm(var2 ~ var1), col = "red")
-}
