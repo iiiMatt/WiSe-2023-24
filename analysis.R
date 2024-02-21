ff19b54172b7c654843cd9a6eb9f52277dc5acb9
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-21 11:11:00 +0100

    Add start of exercise 4 to load all sources  and dataframe

--- analysis.R
+++ analysis.R
@@ -1,3 +1,20 @@
+source("preprocessing.R")
+source("utils.R")
+
+#' preprocessed file is returned as 
+#' "titanic_cleaned.Rds" 
+#' in working directory 
+generate_preprocessed_data()  
+
+dataset = readRDS("titanic_cleaned.Rds")
+
+#' start of exercise 4)
+#' call functions from utils.R (exercise 2) 
+#' and interprete results 
+
+
+
+
 library(gridExtra)
 library(ggplot2)
 library(dplyr)
