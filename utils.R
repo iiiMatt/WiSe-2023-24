7829e7d48894818d5020b58591fca8a86826a9ea
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-21 11:06:48 +0100

    Cleaned utils.R by deleting call of function

--- utils.R
+++ utils.R
@@ -121,7 +121,6 @@ chisq_test_plus_table <- function(kat_var1, kat_var2) {
 Mosaikplot <- function(var1, var2){
   mosaicplot(table(var1, var2 ), main = "Mosaic Plot", shade = TRUE, xlab = deparse(substitute(var1)) , ylab = deparse(substitute(var2)))
 }
-Mosaikplot(dataset$Survived, dataset$Age)
 
 Boxplots_with_Trendline <- function(var1, var2) {
   plot(var1, var2, main = "Boxplot with Trendline",
