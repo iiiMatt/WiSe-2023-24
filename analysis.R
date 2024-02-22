4a1aea0fca1cfc4ebe665a62c9b741aae90dc7d8
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-22 19:27:15 +0100

    ttest survival and age plus organization

--- analysis.R
+++ analysis.R
@@ -18,7 +18,11 @@ dataset = readRDS("titanic_cleaned.Rds")
 #--------------------------------------------------------------------------
 str(dataset)
 #survived, embarked, salutation, deck, side
-# test regarding association among variables, alpha = 0.05
+# test regarding association among variables
+#'alpha = 0.05 but did in total 4 tests 
+#'bonferront adjustment: alpha_adj = 0.05/4 = 0.0125
+
+#######################################################################
 #survived and embarked 
 surv_embarked = chisq_test_plus_table(dataset$Survived, dataset$Embarked)
 surv_embarked[1]
@@ -30,11 +34,12 @@ yes        93         30          217'
 #'embarked in the other cities people who died exceed the survivors
 #'in southhampton almost 2 out of 3 individuals
 surv_embarked[2]
-#'p-value = 1.77e-06 < alpha 
+#'p-value = 1.77e-06 < alpha_adj
 #'statistically significant evidence to support the alternative hypothesis
 #'evidence that there is a significant relationship between 
 #'embarkation and survival
 
+#####################################################################
 #salutation and survival
 surv_salutation = chisq_test_plus_table(dataset$Survived, dataset$Salutation)
 surv_salutation[1]
@@ -52,18 +57,19 @@ kat_var1 Master. Miss. Mr. Mrs.
 #' Men with a title might as well. 
 #' Most men died 
 surv_salutation[2]
-#'p-value = 2.2e-16 < alpha 
+#'p-value = 2.2e-16 < alpha_adj
 #'statistically significant evidence to support the alternative hypothesis
 #'evidence that there is a significant relationship between 
 #'embarkation and survival 
 #'Numbers in contengency show more survival of female passangers 
 #'and likely death for a Mr.
 
+############################################################################
 #survival and deck
-#survival and Side
-surv_deck = chisq_test_plus_table(dataset$Survived, dataset$Deck)
-#warning
 sum(is.na(dataset$Deck))
+#too many NAs to proper analyze 
+############################################################################
+#survival and Side
 sum(is.na(dataset$Side))
 #too many NAs to proper analyze 
 
@@ -73,13 +79,15 @@ sum(is.na(dataset$Side))
 #--------------------------------------------------------------------------
 #'t-test to assess the difference in a metric variable
 #'across two groups defined by a dichotomous variable
-#'alpha set 0.05
+#'alpha = 0.05 but did in total 4 tests 
+#'bonferront adjustment: alpha_adj = 0.05/4 = 0.0125
 
 str(dataset)
 #'useful metric variables:age and fare 
 #'useful dichotomous variable: survived yes or not 
 
-#Fare and Survival 
+##################################################################
+#Fare and Survival Status
 calculateBivariateStats(dataset, "Fare", "Survived")
 #mean and sd regarding groups of survivors or not
 '$no
@@ -98,10 +106,33 @@ mean       sd
 #'even not at all
 #'----------------------------
 #Welch two sample t-test result:
-# p-value = 2.699e-11<alpha
+# p-value = 2.699e-11 < alpha_adj
 #' indicates that the true difference in means between the groups 
 #' is not equal to zero
 # as seen before reagrding the means: seems like it made a difference of how much money 
 #'a person paid to embark the titanic
 #'likely that the rich people and women got rescued first 
 #'suits the society back then
+
+##################################################################
+#Age and Survival Status
+calculateBivariateStats(dataset, "Age", "Survived")
+'$no
+mean       sd 
+30.18488 12.65199 
+
+$yes
+mean       sd 
+28.06629 14.08538 '
+#'Mean of age in both groups around 30 which is young
+#'still a difference of around 2 years between the twio groups
+#'the survivors slightly younger 
+#'might be cause by children who are usually rescued first?
+#'low SD shows that in general the age group did not differ too much
+#'
+#'----------------------------
+#Welch two sample t-test result:
+# p-value = 0.02358 > alpha_adj = 0.0125
+#' not enough evidence against the null hypothesis that the true difference 
+#' in means between the groups is equal to zero
+#' maybe there was not enough data 
