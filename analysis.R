3c739901a0142a79b1c2a84bc8eedfd7536fe146
miamacarena <135004064+miamacarena@users.noreply.github.com>
2024-02-22 18:45:36 +0100

    test and table regarding survival and salutation

--- analysis.R
+++ analysis.R
@@ -34,3 +34,27 @@ surv_embarked[2]
 #'statistically significant evidence to support the alternative hypothesis
 #'evidence that there is a significant relationship between 
 #'embarkation and survival
+
+#salutation and survival
+surv_salutation = chisq_test_plus_table(dataset$Survived, dataset$Salutation)
+surv_salutation[1]
+'kat_var2
+kat_var1 Master. Miss. Mr. Mrs.
+no       17    55 451   26
+yes      23   130  87  102'
+#' around 7 out of 10 Miss. survived 
+#' around 8 out of 20 Mrs. survived 
+#' around 2 out of 10 Mr. survived
+#' around 6 out of 10 Master 
+#' 
+#' possible:
+#' Female passenger got helped to get to the boats.
+#' Men with a title might as well. 
+#' Most men died 
+surv_salutation[2]
+#'p-value = 2.2e-16 < alpha 
+#'statistically significant evidence to support the alternative hypothesis
+#'evidence that there is a significant relationship between 
+#'embarkation and survival 
+#'Numbers in contengency show more survival of female passangers 
+#'and likely death for a Mr.
