c850ffc83ef1644ab5e3b1827164d52d99cb049d
Neo H <neo.hong@tu-dortmund.de>
2024-02-23 21:23:48 +0100

    Generated documentation

--- utils.R
+++ utils.R
@@ -3,24 +3,33 @@ library(gridExtra)
 library(ggplot2)
 library(dplyr)
 
-#'2a- i)
+#' Descriptive statistics for a metric variable
+#'
 #' Returns some descriptive statistics for a metric variable in form of a
 #' dataframe for simple formatting. All NA values are removed before
 #' calculation.
+#'
+#' Task 2a i.
 #' @param x A metric variable.
 #' @return A one row dataframe with the different statistics in columns.
-describe_metric <- function(x){
-  data.frame(Min = min(x, na.rm=T), erstesQuartil =quantile(x, 0.25, na.rm=T) , 
-             Median = median(x, na.rm=T), Mean =mean(x, na.rm=T), 
-             SD = sd(x, na.rm=T), drittesQuartil =quantile(x, 0.75, na.rm=T),
-             Max = max(x, na.rm=T),
-             NAs = sum(is.na(x))
-             )
+describe_metric <- function(x) {
+    data.frame(
+        Min = min(x, na.rm = T), erstesQuartil = quantile(x, 0.25, na.rm = T),
+        Median = median(x, na.rm = T), Mean = mean(x, na.rm = T),
+        SD = sd(x, na.rm = T), drittesQuartil = quantile(x, 0.75, na.rm = T),
+        Max = max(x, na.rm = T),
+        NAs = sum(is.na(x))
+    )
 }
 
-#'2a-ii)
+#' Descriptive statistics for a categorial variable
+#'
 #' Prints descriptive statistics for a categorial variable x and additional
-#' ones for ordered factors.
+#' ones for ordered factors. Namely the calculated statistics are the
+#' statistical mode, empirical entropy and phi measure of dispersion and
+#' optionally the median and quartiles.
+#'
+#' Task 2a ii.
 #' @param x A factor (may be ordered).
 #' @param name The name of the factor. By default whatever was passed to x so
 #' for describe_categorial(data$Pclass) name would default to "data$Pclass".
@@ -55,40 +64,43 @@ describe_categorial <- function(
         cat("Median:                             ", med, "\n")
         cat("Upper quartile:                     ", q3, "\n")
     }
-  }
+}
 
-#'2a-iii)
-#' Calcualates and prints the results of descriptive bivariate statistics of a 
-#' contengency table  and a chi squared test for two categorical variables 
+#' Bivariate descriptive statistics for categorial variables
+#'
+#' Calcualates and prints the results of descriptive bivariate statistics of a
+#' contengency table  and a chi squared test for two categorical variables.
+#'
+#' Task 2a iii.
 #' @param kat_var1 A kategorical variable
-#' @param kat_var2 A second kategorical variable with the same length as the first 
+#' @param kat_var2 A second kategorical variable with the same length as the first
 chisq_test_plus_table <- function(kat_var1, kat_var2) {
-  # check if both variables are categorical (purpose of the excercise)
-  if (!is.factor(kat_var1) || !is.factor(kat_var2)) {
-    stop("Both variables need to be categorical.")
-  }
-  # contingency table for output and test
-  contingency_table <- table(kat_var1, kat_var2)
+    # check if both variables are categorical (purpose of the excercise)
+    if (!is.factor(kat_var1) || !is.factor(kat_var2)) {
+        stop("Both variables need to be categorical.")
+    }
+    # contingency table for output and test
+    contingency_table <- table(kat_var1, kat_var2)
 
-  # chi-squared-test to output results
-  chi_squared_result <- chisq.test(contingency_table)
+    # chi-squared-test to output results
+    chi_squared_result <- chisq.test(contingency_table)
 
-  #cat output to explain results/ output plus print results on console
-  
-  cat("The results contain a contingency table and a chi squared test regarding the following hypotheses:\n
+    # cat output to explain results/ output plus print results on console
+
+    cat("The results contain a contingency table and a chi squared test regarding the following hypotheses:\n
       Null Hypothesis (H0): There is no association between the two categorical variables. \n
       Alternative Hypothesis (H1): There is an association between the two categorical variables.\n
       If p-value < alpha: Reject the null hypothesis. \n
       If p-value >= alpha: Fail to reject the null hypothesis.")
-  print(contingency_table)
-  print(chi_squared_result)
-  
-  return( list(
-    contingency_table = contingency_table, 
-    chi_squared_result = chi_squared_result))
+    print(contingency_table)
+    print(chi_squared_result)
+
+    return(list(
+        contingency_table = contingency_table,
+        chi_squared_result = chi_squared_result
+    ))
 }
 
-#'2a-iv)
 #' Calculate Descriptive and Inferential Statistics
 #' for a Metric and a Dichotomous Variable
 #'
@@ -99,12 +111,15 @@ chisq_test_plus_table <- function(kat_var1, kat_var2) {
 #' two groups, such as comparing the average fare paid by passengers who
 #' survived vs. those who did not.
 #'
+#' Task 2a iv.
+#'
 #' @param data A dataframe containing the dataset.
 #' @param metricVar The name of the metric variable as a string.
 #' @param dichotomousVar The name of the dichotomous
 #'  variable as a string.
 #'
 #' @example calculateBivariateStats(df, "Fare", "Survived")
+#' @export
 calculateBivariateStats <- function(data, metricVar, dichotomousVar) {
     # Ensure the dichotomous variable is a factor
     data[[dichotomousVar]] <- as.factor(data[[dichotomousVar]])
@@ -127,12 +142,13 @@ calculateBivariateStats <- function(data, metricVar, dichotomousVar) {
     print(tTestResult)
 }
 
-#'2a-v)
 #' Visualize Categorical Variables Relation to Survival on Titanic
 #'
 #' This function creates visualizations for the relationship between
 #' survival and three categorical variables:
 #' Passenger Class (Pclass), Sex, and Embarkation Point (Embarked)
+#'
+#' Task 2a v.
 #'
 #' @param df A dataframe
 #' @return Returns a grid of three plots visualizing the survival rates by
@@ -145,8 +161,10 @@ visualize_categorical_variables <- function(df) {
     # Plot for Pclass
     plot_pclass <- ggplot(df, aes(x = Pclass, fill = Survived)) +
         geom_bar(position = "fill") +
-        labs(title = "Survival Rate by Passenger Class",
-             x = "Passenger Class", y = "Survival Rate") +
+        labs(
+            title = "Survival Rate by Passenger Class",
+            x = "Passenger Class", y = "Survival Rate"
+        ) +
         scale_fill_brewer(palette = "Set1") +
         theme_minimal()
 
@@ -160,8 +178,10 @@ visualize_categorical_variables <- function(df) {
     # Plot for Embarked (Na is "Unknown")
     plot_embarked <- ggplot(df, aes(x = Embarked, fill = Survived)) +
         geom_bar(position = "fill") +
-        labs(title = "Survival Rate by Embarkation Point",
-             x = "Embarkation Point", y = "Survival Rate") +
+        labs(
+            title = "Survival Rate by Embarkation Point",
+            x = "Embarkation Point", y = "Survival Rate"
+        ) +
         scale_fill_brewer(palette = "Set3") +
         theme_minimal()
 
@@ -169,23 +189,30 @@ visualize_categorical_variables <- function(df) {
     grid.arrange(plot_pclass, plot_sex, plot_embarked, nrow = 3)
 }
 
-#'2a - vi)
-#' Additional functions suitable for description and visualization
+#' Mosaicplot
+#'
 #' The function takes two variables, var1 and var2, as inputs. It then creates a mosaic plot using these variables
-#' @param var1 one Variable
-#' @param var2 another vaiable
+#'
+#' Task 2a vi.
+#' @param var1 One variable
+#' @param var2 Another variable
 #' @return a mosaicplot
-Mosaikplot <- function(var1, var2){
-  mosaicplot(table(var1, var2 ), main = "Mosaic Plot", shade = TRUE, xlab = deparse(substitute(var1)) , ylab = deparse(substitute(var2)))
+Mosaikplot <- function(var1, var2) {
+    mosaicplot(table(var1, var2), main = "Mosaic Plot", shade = TRUE, xlab = deparse(substitute(var1)), ylab = deparse(substitute(var2)))
 }
 
+#' Boxplots with Trendline
+#'
 #' The function takes two variables, var1 and var2, as inputs. It then creates a boxplot of var2 against var1, with a trendline added.
-#' @param var1 one Variable
-#' @param var2 another vaiable
+#'
+#' Task 2a vi.
+#' @param var1 One variable
+#' @param var2 Another variable
 #' @return a boxplot with trendlinie
-
 Boxplots_with_Trendline <- function(var1, var2) {
-  plot(var1, var2, main = "Boxplot with Trendline",
-       xlab = deparse(substitute(var1)), ylab = deparse(substitute(var2)))
-  abline(lm(var2 ~ var1), col = "red")
+    plot(var1, var2,
+        main = "Boxplot with Trendline",
+        xlab = deparse(substitute(var1)), ylab = deparse(substitute(var2))
+    )
+    abline(lm(var2 ~ var1), col = "red")
 }
