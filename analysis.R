3cad1ae86647dcd5d9848d03db1c826b83ec17e9
helen-mza <156697847+helen-mza@users.noreply.github.com>
2024-02-23 09:51:21 +0100

    Update analysis.R
    Describe a metric variable with the example of the age

--- analysis.R
+++ analysis.R
@@ -27,18 +27,4 @@ titanic_age <- describe_metric(titanic_cleaned$Age)
 #We can see that the youngest passenger was only 0.42 (less than 1 year old) and the oldest 80 years old.
 #The average age on the ship is 30 with a standard derivation of 13.25. 
 
-#'2a- ii)
-#'descriptive statistics for a categorial variable
-#'as an example we can use the categorial variable 
 
-titanic_deck <- describe_categorial(titanic_cleaned$Deck)
-
-#Descriptive statistics for the factor titanic_cleaned$Deck (NA values removed) 
-#Levels:                              A, B, C, D, E, F, G, T 
-#Not available:                       687 of 891 values
-#Statistical mode:                    C 
-#Boltzmann's empirical entropy:       0.8453196 
-#Phi dispersion for categorial data:  0.6775701 
-
-#We can see that we have 8 different levels where of the mode is deck C. Interpreted it means that most of the
-#passengers were located on deck C. However only 204 passengers data were available.
